d3ed4f35d9597de47e016af50a2c62d823f893d6
Rupesh <rupesh.bogadapati@rakuten.com>
2017-06-15 11:25:08 +0530

    Made changes to start

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,4 @@
 var userCtrl = require('./controllers/user');
-var orderCtrl = require('./controllers/order');
 var referralCtrl = require('./controllers/referral');
 var config = require('./config/config');
 var moment = require('moment');
@@ -10,10 +9,6 @@ module.exports = function(app) {
     app.post('/register', userCtrl.signUp);
     app.post('/login', userCtrl.login);
     app.get('/api/users',ensureAuthenticated, userCtrl.getUsers);
-
-
-    //order apis
-    app.post('/api/order/new',ensureAuthenticated,orderCtrl.newOrder)
 
     //referral api
     app.post('/api/referral/new', ensureAuthenticated, referralCtrl.newReferral);
@@ -35,7 +30,7 @@ function ensureAuthenticated(req, res, next) {
       if (payload.exp <= moment().unix()) {
         return res.status(401).send({ message: 'Token has expired' });
       }
-      
+
       User.getUserById(payload.sub, function(err, user){
           if (err) {
                 return res.status(401).send({ message: 'Invalid token' });
